9c2fbd341819db9fc580677d6d9e35300d828f53
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-05-02 02:26:58 -0300

    first commit

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,25 @@
 defmodule GenReport do
-
-  def build do
-    # TO DO
+  def build(path) do
+    usuarios =  read(path)
+    |> File.stream!()
+    |> Stream.map(fn line -> parse_line(line) end)
   end
+
+  def read(path) do
+    case File.read(path) do
+      {:ok, binary} ->
+        binary
+
+      {:error, reason} ->
+        raise File.Error, reason: reason, action: "read file", path: IO.chardata_to_string(path)
+    end
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    # |> String.split(",")
+    # |> List.update_at(2, &String.to_integer/1)
+  end
+
 end
